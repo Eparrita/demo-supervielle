f8b0a4b58dbb61519df89f23b0cb1237c96b0e16
Soluciones A.G.E.S <solucionesages@aqui-es.com>
2019-06-10 19:43:25 -0300

    Updates

---
+++ precache-manifest.3f852b3ae0c14c6e119e148b84b5f83c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f564974ea084061f83a70ee8f05ee3",
+    "url": "/demo-supervielle/index.html"
+  },
+  {
+    "revision": "aa112bcd50c8fddc5234",
+    "url": "/demo-supervielle/static/css/main.52d05463.chunk.css"
+  },
+  {
+    "revision": "634d66ad4133342b73c4",
+    "url": "/demo-supervielle/static/js/2.cd77abba.chunk.js"
+  },
+  {
+    "revision": "aa112bcd50c8fddc5234",
+    "url": "/demo-supervielle/static/js/main.ea09d6d8.chunk.js"
+  },
+  {
+    "revision": "9182a18b379b3cc5ad85",
+    "url": "/demo-supervielle/static/js/runtime~main.664099c6.js"
+  },
+  {
+    "revision": "cb370b808d3fd2a860d135973fd0eb80",
+    "url": "/demo-supervielle/static/media/background-cotizaciones.cb370b80.jpg"
+  },
+  {
+    "revision": "2ed1fa92f3571427cd3725290949748e",
+    "url": "/demo-supervielle/static/media/logo.2ed1fa92.svg"
+  }
+]);
